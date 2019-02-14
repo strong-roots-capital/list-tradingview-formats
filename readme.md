b8a97f821980ea4c33d8d4f75d156c71cdaa39db
Eric Crosson <eric.s.crosson@utexas.edu>
2019-02-14 11:15:58 -0600

    Add typedoc

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # list-tradingview-formats [![Build status](https://travis-ci.org/strong-roots-capital/list-tradingview-formats.svg?branch=master)](https://travis-ci.org/strong-roots-capital/list-tradingview-formats) [![npm version](https://img.shields.io/npm/v/@strong-roots-capital/list-tradingview-formats.svg)](https://npmjs.org/package/@strong-roots-capital/list-tradingview-formats) [![codecov](https://codecov.io/gh/strong-roots-capital/list-tradingview-formats/branch/master/graph/badge.svg)](https://codecov.io/gh/strong-roots-capital/list-tradingview-formats)
 
-> Generate a list of timeframes in TradingView format
+> Generate a list of timeframes in Trading View format
 
 ## Install
 
--- src/list-tradingview-formats.ts
+++ src/list-tradingview-formats.ts
@@ -5,24 +5,10 @@
 
 const fill = require('fill-range')
 
-// `Trading View format` of timeframes is hereby defined as follows:
-
-// - Minutes are expressible by an integer from 1 to 1440
-// - Hours are expressible by an integer from 1 to 24, followed by a
-//   capital `H`
-// - Days are expressible by an integer from 1 to 365, followed by a
-//   capital `D`
-// - Weeks are expressible by an integer from 1 to 52, followed by a
-//   capital `W`
-// - Months expressible by an integer from 1 to 12, followed by a
-//   capital `M`
-// - Hours, Days, Weeks, Months may all have an implicit quantifier of
-//   `1`, e.g. `H` is equivalent to `1H`
-//
-// Any other string is not a timeframe in `Trading View format`.
-
 /**
- * TODO: document
+ * Return list of all valid Trading View-formatted timeframes.
+ *
+ * @returns List of all valid Trading View-formatted timeframes.
  */
 export default function listTradingviewFormats(): string[] {
     let timeframes: string[] = []
